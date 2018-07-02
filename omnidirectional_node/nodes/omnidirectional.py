73d32192700c5140276cd6b604fb319e24110db7
Stuart Leal Q <stuart.leal23@gmail.com>
2018-07-02 10:41:45 -0600

    Basic structure for node

--- omnidirectional_node/nodes/omnidirectional.py
+++ omnidirectional_node/nodes/omnidirectional.py
@@ -1,2 +1,27 @@
 #!/usr/bin/env python
 
+"""
+Ros node for Arcos Lab Omnidirectional robot.
+"""
+
+__author__ = "stuart.leal@ucr.ac.cr (Stuart Leal)"
+
+import roslib; roslib.load_manifest("omnidirectional_node")
+import rospy
+
+from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from tf.broadcaster import TransformBroadcaster
+
+# import from serial com script
+
+class OmnidirectionalNode:
+
+    def __init__(self):
+        """ Start up configuration for Omnidirectional Robot. """
+        rospy.init_node('omnidirectional')
+
+        self.port = rospy.get_param("port", "/dev/ttyACM0")
+        rospy.loginfo("Using port: %s"%(self.port))
+
