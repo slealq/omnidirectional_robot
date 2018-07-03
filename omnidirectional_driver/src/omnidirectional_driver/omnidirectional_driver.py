eba8980a1b607b84ec28db7bb19f128f5a107ddc
Stuart Leal Q <stuart.leal23@gmail.com>
2018-07-02 21:38:59 -0600

    Correction in file name, functional set motors function

---
+++ omnidirectional_driver/src/omnidirectional_driver/omnidirectional_driver.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+"""
+This file is a generic driver for the Arcos Lab
+Omnidirectional Robot.
+"""
+
+__author__ = "stuart.leal@ucr.ac.cr (Stuart Leal)"
+
+import serial
+import time
+
+BASE_WIDTH = 290 # milimiters
+MAX_SPEED = 282.74 # milimeters / second
+
+class omni():
+
+    def __init__(self, port="/dev/ttyACM0"):
+        self.port = serial.Serial(port, 115200, timeout=1)
+
+    def test_connection(self):
+        """Test connection"""
+
+    def set_motors(self, x, y, omega):
+        """Basic function that will send commands"""
+
+        # flush clean the port
+        self.port.flush()
+
+        # write m code for motos
+        self.port.write("m".encode('utf-8'))
+        ack_code = self.port.read().decode('utf-8')
+
+        # write vel commands
+        self.port.write("0.1 0.1 0.1 0.1\n".encode('utf-8'))
+        ack_code = self.port.readline().decode('utf-8')
+
+    def close_connection(self):
+        """Close the connection"""
+        if(self.port.isOpen()):
+            self.port.close()
