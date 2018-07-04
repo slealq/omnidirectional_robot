cb30c104065e8b56e89255a8249c385e4b22a88a
Stuart Leal Q <stuart.leal23@gmail.com>
2018-07-04 14:44:07 -0600

    Add first lines for spin code, with while loop.

--- omnidirectional_node/nodes/omnidirectional.py
+++ omnidirectional_node/nodes/omnidirectional.py
@@ -14,7 +14,7 @@ from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from tf.broadcaster import TransformBroadcaster
 
-# import from serial com script
+from omnidirectional_driver.omnidirectional_driver import omni, BASE_WIDTH, MAX_SPEED
 
 class OmnidirectionalNode:
 
@@ -22,6 +22,27 @@ class OmnidirectionalNode:
         """ Start up configuration for Omnidirectional Robot. """
         rospy.init_node('omnidirectional')
 
+        # set communication port
         self.port = rospy.get_param("port", "/dev/ttyACM0")
         rospy.loginfo("Using port: %s"%(self.port))
 
+        # use driver for robot operation
+        self.robot = omni(self.port)
+
+        # subscribe to cmd vel topic
+        rospy.Subscriber("cmd_vel", Twist, self.cmd_vel_cb)
+
+    def spin(self):
+        """ Spin function to call """
+
+        then = rospy.Time.now()
+
+        r = rospy.Rate(5)
+
+        self.robot.requestScan()
+
+        while not rospy.is_shutdown():
+            # get motor encoder values
+
+            # send updated movement codes
+            self.robot.set_motors(self.cmd_vel[0], )
